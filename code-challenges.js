585c433b9d9518ce24d135bc1316a39b8d36c475
sethcrist <sethcrist50@gmail.com>
2024-02-09 17:07:58 -0800

    finished assessment

--- code-challenges.js
+++ code-challenges.js
@@ -26,24 +26,68 @@ const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 17
 
+// psudo code:
+    // input: array of numbers
+    // for loop to itterate over array
+    // add numbers
+    // output: sum of array
+const sumOfNumbers = (array) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+         sum += array[i]
+    }
+    return sum
+}
+console.log(sumOfNumbers(padres1998WorldSeriesRuns));
 // Explain your code:
+    // line 34 is our function with an array as a parameter, 
+    // line 35 is our variable to store the added numbers, 
+    // line 36 is a for loop that itterates over each element(i++ and .length) in the array starting at the 0 index(2), 
+    // line 37 is a short hand notation of sum = sum + array that adds up the elements starting at the first index, 
+    // line 39 returns the value of each element added up.
 
 // --------------------2) Create a function that takes in a string and  returns the string with all the characters in reversed order. Use the test variable provided below.
 
 const lesMiserables = "Jean Valjean"
 // Expected output: "naejlaV naeJ"
-
 const lionKing = "Simba"
 // Expected output: "abmiS"
 
+// psudo code:
+    // input: string
+    // .split("") to split the string into seperate characters
+    // .reverse() to reverse the elements in the arrray
+    // .join("") to change the array back into a string
+    // output: the string reversed
+
+const reversed = (str) => {
+    return str.split("").reverse().join("")
+}
+ console.log(reversed(lesMiserables));
+
 // Explain your code:
+    //created a function that took a string as a parameter and returned it using the .split(), .reverse() and .join() method reversed
 
 // --------------------3) Create a function that takes in an array and returns an array with only the items from the third, fourth, and fifth indexes. Use the test variable provided below.
 
 const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
 // Expected output: [42, 5, -5]
-
 const cohortList = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"]
 // Expected output: ["Delta", "Echo", "Foxtrot"]
 
+// psudo code: 
+    // input: array
+    // variable to hold new array
+    // for loop to itterate over the array
+    // .push to populate our empty array
+    // output: new array with items from the 3rd, 4th, and 5th index
+const thridFourthFifth = (array) => {
+    let newArray = []
+    for (let i = 3; i <= 5; i++) {
+        newArray.push(array[i])
+    }
+    return newArray
+}
+console.log(thridFourthFifth(stockExchange));
 // Explain your code:
+    // created a function with a variable named newArray that is an empty array. this will create our new array. our for loop itterates over the array starting at the 3rd index(i = 3) and increase by one until we get the the 5th index of the array (i <= 5) where it will stop. The .push() method will populate that data in our variable newArray.
--- coding-concepts.js
+++ coding-concepts.js
@@ -16,40 +16,41 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
-// console.log(cohort.length)
+ console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: will give us 10 because the .length method counts all the characters in the string including the space.
+// b) Verify and explain: my answer was correct, the .length method counts the characters and the space between the characters.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+ console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will print the character "o" in the terminal because it is at the 4th index of the string
+// b) Verify and explain: i was correct greeting[4] outputs "o" in the terminal because it is at the 4th index
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+ console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will log Ruby in the terminal because it is at the first index of the languages array
+// b) Verify and explain: correct, because javascript is at the 0 index and ruby is at the 1st index of the array
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+ //console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:this will output saturday, sunday in all caps on the termianl
+// b) Verify and explain: incorrect: and i think its because its an array and not a string
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+ console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: i think this will log the datatypes as being strings and that there is 33 characters in this array. and that is excluding the "" 
+// b) Verify and explain: incorrect because the .length returns the numeric value and so dataTypes.length will log: number
+
